f25772326b0385ce16b8ea0af0b405c15c0a6cb3
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-09-20 11:04:19 +0500

    session user created

---
+++ src/main/java/com/pdp/yourmeal/config/SessionUser.java
@@ -0,0 +1,34 @@
+package com.pdp.yourmeal.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 14/September/2024  13:36
+ **/
+@Component
+@SuppressWarnings("unused")
+public class SessionUser {
+
+//    public CustomUserDetails user() {
+//        SecurityContext securityContext = SecurityContextHolder.getContext();
+//        Authentication authentication = securityContext.getAuthentication();
+//        Object principal = authentication.getPrincipal();
+//        if (principal instanceof UserDetails ud)
+//            return (CustomUserDetails) ud;
+//        return null;
+//    }
+
+    public Long id() {
+//        CustomUserDetails user = user();
+//        if (Objects.isNull(user))
+//            return -1L;
+//        return user.getId();
+    }
+}
